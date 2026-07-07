6cdf0a50c60e22a78c3702429a50c94f9267ebca
Crystal <cawash17@gmail.com>
2026-07-07 01:35:31 -0400

    part 5:advanced task

--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -33,3 +33,33 @@ async function promisesDemo() {
 promisesDemo();
 
 // Streams for large files- log first 40 chars of each chunk
+//create path for stream txt called largefile.txt
+const largeFile = path.join(__dirname, "sample-files", "largefile.txt");
+//create writeStream loop to write a line x100
+const writeStream = fs.createWriteStream(largeFile);
+for (let i = 0; i < 100; i++) {
+  writeStream.write("This is a lot of text\n");
+}
+writeStream.end();
+
+//handles what occurs after stream write is complete
+writeStream.on("finish", () => {
+  //read stream in chunks
+  //use highWaterMark to set chunk size
+  const readStream = fs.createReadStream(largeFile, {
+    encoding: "utf8",
+    highWaterMark: 1024,
+  });
+  // data event, console log first 40 char chunk read (use chunk.substring(0,40))
+  readStream.on("data", (chunk) => {
+    console.log("Read chunk:", chunk.substring(0, 40));
+  });
+  //end event console log finished message at the end of the read
+  readStream.on("end", () => {
+    console.log("Finished reading large file with streams.");
+  });
+  //error event handle error case
+  readStream.on("error", (err) => {
+    console.log("Error reading file:", err.message);
+  });
+});

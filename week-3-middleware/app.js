08f66ba96e3e5db3e6ea7c5f65bc4fe9766e0060
Crystal <cawash17@gmail.com>
2026-07-21 01:17:17 -0400

    assignment3b steps 9-13 dog rescue middleware

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,15 +1,54 @@
 const express = require("express");
 const dogsRouter = require("./routes/dogs");
+const { randomUUID } = require("crypto");
 
 const app = express();
 
 // Assignment 3b and 3c ask you to add middleware in this file.
 
+//request id
+app.use((req, res, next) => {
+  req.requestId = randomUUID();
+  res.setHeader("X-Request-Id", req.requestId);
+  next();
+});
 
+//logger
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}]: ${req.method} ${req.path} (${req.requestId})`);
+  next();
+});
 
+//security headers
 
-app.use("/", dogsRouter);// Do not remove this line
+//json parsing
+app.use(express.json());
 
+//static file
+app.use(express.static("week-3-middleware/public"));
+
+//content-type validation
+
+//routes
+app.use("/", dogsRouter); // Do not remove this line
+
+//not found(404)
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Route not found",
+    requestId: `"${req.requestId}"`,
+  });
+});
+
+//error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    error: "Internal Server Error",
+    requestId: `"${req.requestId}"`,
+  });
+});
 
 if (require.main === module) {
   app.listen(3000, () => {
@@ -18,4 +57,3 @@ if (require.main === module) {
 }
 
 module.exports = app;
-

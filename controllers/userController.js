561e39fb95cf0dc45874cf6ac3a10b936074b9b6
Crystal <cawash17@gmail.com>
2026-09-09 18:35:11 -0400

    add registration reCAPTCHA verification and test bypass header

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 const util = require("util");
 const prisma = require("../db/prisma");
 const jwt = require("jsonwebtoken");
-
+const { StatusCodes } = require("http-status-codes");
 const scrypt = util.promisify(crypto.scrypt);
 
 async function hashPassword(password) {
@@ -39,6 +39,38 @@ const setJWTCookie = (req, res, user) => {
 };
 
 async function register(req, res, next) {
+  let isPerson = false;
+  if (req.body.recaptchaToken) {
+    const token = req.body.recaptchaToken;
+    const params = new URLSearchParams();
+    params.append("secret", process.env.RECAPTCHA_SECRET);
+    params.append("response", token);
+    params.append("remoteip", req.ip);
+    const response = await fetch(
+      "https://www.google.com/recaptcha/api/siteverify",
+      {
+        method: "POST",
+        body: params.toString(),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    );
+    const data = await response.json();
+    if (data.success) isPerson = true;
+    delete req.body.recaptchaToken;
+  } else if (
+    process.env.RECAPTCHA_BYPASS &&
+    req.get("X-Recaptcha-Test") === process.env.RECAPTCHA_BYPASS
+  ) {
+    isPerson = true;
+  }
+  if (!isPerson) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "Bot verification failed. Please complete the reCAPTCHA.",
+    });
+  }
+
   if (!req.body) req.body = {};
   const { error, value } = userSchema.validate(req.body, {
     abortEarly: false,
